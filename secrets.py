a84fae79895855df9fb3fa85d7618589dfc692fb
Michael Torio <michaeltorio95@gmail.com>
2021-06-25 21:40:28 -0700

    Second Commit

---
+++ secrets.py
@@ -0,0 +1,11 @@
+#users spotify account name
+def spotify_user_id():
+    return ''
+
+#Spotify token generated in spotify's api console. Expires every hour
+def spotify_token():
+    return ''
+
+#lastfm api key
+def last_fm_api_key():
+    return ''
